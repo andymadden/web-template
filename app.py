86e221838f8481fcfb659ed7621c0a1531b6b7da
Andrew Madden <48344421+andymadden@users.noreply.github.com>
2021-08-15 06:10:15 -0500

    Updated strings for loading css/js files
    Updated some strings

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ def index():
 @app.route("/css/<file>")
 def css(file):
     content = ""
-    with open("css/<file>") as css_file:
+    with open(f"css/{file}") as css_file:
         content = css_file.read()
     return Response(content, content_type="text/css")
 
 @app.route("/js/<file>")
 def js(file):
     content = ""
-    with open("js/<file>") as js_file:
+    with open(f"js/{file}") as js_file:
         content = js_file.read()
     return Response(content, content_type="text/javascript")
 
